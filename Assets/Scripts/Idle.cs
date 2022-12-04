6f2cac7666ff3044b136f568385a65cc6f0855a2
RomanMartinez <roman.martinezcristaldo@gmail.com>
2022-12-04 19:23:14 -0300

    correciones

---
+++ Assets/Scripts/Idle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : IState
+{
+    FSMachine _fsm;
+
+    public Idle(FSMachine fsm)
+    {
+        _fsm = fsm;
+    }
+
+    public void OnStart()
+    {
+        Debug.Log("Entre a Idle");
+    }
+
+    public void OnUpdate()
+    {
+        Debug.Log("Estoy en Idle");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _fsm.ChangeState(AgentStates.Patrol);
+        }
+    }
+
+    public void OnExit()
+    {
+
+    }
+}
